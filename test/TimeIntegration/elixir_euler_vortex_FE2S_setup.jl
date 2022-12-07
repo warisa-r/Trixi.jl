965d3248a02ce1d618495e5c024442c811be735f
Daniel <daniel.doehring@rwth-aachen.de>
2022-12-07 16:46:15 +0100

    Add elixiers for testcases

--- test/TimeIntegration/elixir_euler_vortex_FE2S_setup.jl
+++ test/TimeIntegration/elixir_euler_vortex_FE2S_setup.jl
@@ -60,11 +60,10 @@ solver = DGSEM(polydeg=6, surface_flux=surf_flux)
 
 coordinates_min = (-20.0, -20.0)
 coordinates_max = ( 20.0,  20.0)
+NumCells = 80
+cells_per_dimension = (NumCells, NumCells)
 
-Refinement = 6
-mesh = TreeMesh(coordinates_min, coordinates_max,
-                initial_refinement_level=Refinement,
-                n_cells_max=30_000)
+mesh = StructuredMesh(cells_per_dimension, coordinates_min, coordinates_max)
 
 semi = SemidiscretizationHyperbolic(mesh, equations, initial_condition, solver)
 
@@ -84,26 +83,123 @@ analysis_callback = AnalysisCallback(semi, interval=analysis_interval, save_anal
 
 alive_callback = AliveCallback(analysis_interval=analysis_interval)
 
+# NOTE: The timesteps in the PERK publication where {10, 5, 2.5, 1.25} * 10^{-4}
+if surf_flux == flux_lax_friedrichs
+  stepsize_callback = StepsizeCallback(cfl=0.36) # this gives roughly 27 * 10^{-4}
+elseif surf_flux == flux_hll
+  stepsize_callback = StepsizeCallback(cfl=0.71) # this gives roughly 53 * 10^{-4}
+end
+
 callbacks = CallbackSet(summary_callback,
                         analysis_callback, alive_callback)
+                        #stepsize_callback)
+
 
 ###############################################################################
 # run the simulation
 
-NumCells = 2^Refinement
+#=
+# PERK papers use (SSP[?]) RK2 for comparison
+sol = solve(ode, SSPRK22(),
+            #dt=1.0, # solve needs some value here but it will be overwritten by the stepsize_callback
+            dt = 4.8e-03,
+            save_everystep=false, callback=callbacks);
+=#
+
+# Testruns: Check if spatial accuracy limits now
+#=
+sol = solve(ode, CarpenterKennedy2N54(williamson_condition=false),
+            #dt=1.0, # solve needs some value here but it will be overwritten by the stepsize_callback
+            dt = 0.75e-2,
+            save_everystep=false, callback=callbacks);
+=#
+
+# Control case: See if 2x2 is sufficient to estimate 80x80
+NumStages = 4
+CFL_Opt             = 1.0 # By definition
+CFL_Internal        = 1.0
+CFL_ConvergenceStudy = 1.4 * 0.83 # 2x2
+
+#=
+CFL_ConvergenceStudy = 0.99 # 3x3
+CFL_ConvergenceStudy = 0.99 # 4x4
+CFL_ConvergenceStudy = 0.99 # 5x5
+CFL_ConvergenceStudy = 1.0 # 6x6
+CFL_ConvergenceStudy = 1.0 # 7x7
+CFL_ConvergenceStudy = 0.99 # 8x8
+=#
+
+
+# 26 
+#=
+NumStages = 26
+CFL_Opt = 0.95535463700870915
+
+CFL_Internal = 0.79 # Lebedev
+CFL_Internal = 0.04 # Successive Intermediate classic RK
+CFL_Internal = 0.8 # Classic RK
+
+CFL_ConvergenceStudy = 1
+
+
+# 52 
+NumStages = 52
+CFL_Opt = 0.93135613167399789
+
+CFL_Internal = 0.7 # Lebedev
+CFL_Internal = 0.01 # Successive Intermediate classic RK
+CFL_Internal = 0.48 # Classic RK
+
+CFL_ConvergenceStudy = 1
+=#
+
+
+# 104
+#=
+NumStages = 104
+CFL_Opt = 0.91021259292341095
+
+CFL_Internal = 0.28 # Lebedev
+CFL_Internal = 0.004 # Successive Intermediate classic RK
+CFL_Internal = 0.23 # Classic RK
+
+CFL_ConvergenceStudy = 1
+=#
+
+NumStageRef = 16
+dtRef = 2.29160435257381323
+
+NumStageRef = 4
+
+NumCellsRef = 2
+dtRef = 0.419013938898388005 # 2x2
+
+#=
+NumCellsRef = 3
+dtRef = 0.338754492229782045 # 3x3
+
 NumCellsRef = 4
+dtRef = 0.255777720361038519 # 4x4
 
-NumBaseStages = 4
-NumDoublings = 2
+NumCellsRef = 5
+dtRef = 0.204524988307639433 # 5x5
 
-dtRefBase = 0.255777720361038519
+NumCellsRef = 6
+dtRef = 0.168881459738258854 # 6x6
 
-CFL = 0.99
+NumCellsRef = 7
+dtRef = 0.145751630958147865 # 7x7
 
-dtOptMin = dtRefBase / (NumCells/NumCellsRef) * CFL
+NumCellsRef = 8
+dtRef = 0.127459716796329298 # 8x8
+=#
 
-ode_algorithm = PERK_Multi(NumBaseStages, NumDoublings, 
-                "/home/daniel/Desktop/git/MA/EigenspectraGeneration/Spectra/2D_ComprEuler_Vortex/4Cells/")
+dtOptMin = dtRef * (NumStages / NumStageRef) / (NumCells/NumCellsRef) * CFL_Opt * CFL_Internal * CFL_ConvergenceStudy
+
+#ode_algorithm = FE2S(NumStages, "/home/daniel/Desktop/git/MA/EigenspectraGeneration/Spectra/2D_ComprEuler_Vortex/")
+
+ode_algorithm = PERK(NumStages, 
+                "/home/daniel/Desktop/git/MA/EigenspectraGeneration/Spectra/2D_ComprEuler_Vortex/")
 
 sol = Trixi.solve(ode, ode_algorithm,
                   dt = dtOptMin,
