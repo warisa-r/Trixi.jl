9c55f1881073ca465b24f0500dfc57c03ef3253b
Daniel_Doehring <daniel.doehring@rwth-aachen.de>
2023-07-12 10:14:26 +0200

    BC for PERK (1D) correction

--- src/solvers/dgsem_tree/dg_1d.jl
+++ src/solvers/dgsem_tree/dg_1d.jl
@@ -749,7 +749,6 @@ end
 # TODO: Probably not working yet
 function calc_boundary_flux!(cache, t, boundary_condition::BoundaryConditionPeriodic,
                              mesh::TreeMesh{1}, equations, surface_integral, dg::DG,
-                             level_info_elements_acc::Vector{Int64}, 
                              level_info_boundaries_acc::Vector{Int64})
   @assert isempty(eachboundary(dg, cache))
 end
