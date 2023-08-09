eecffa97cc2b58d6a793dd77aadb3ee5cd7ee391
Daniel_Doehring <daniel.doehring@rwth-aachen.de>
2023-08-09 16:05:12 +0200

    Test Adv-Diff-Reac

---
+++ examples/tree_1d_dgsem/elixir_advection_diffusion_reaction_amr.jl
@@ -0,0 +1,138 @@
+
+using OrdinaryDiffEq, Plots
+using Trixi, LinearAlgebra
+
+###############################################################################
+# semidiscretization of the linear advection diffusion equation
+
+advection_velocity = 1
+equations = LinearScalarAdvectionEquation1D(advection_velocity)
+diffusivity() = 1e-4
+equations_parabolic = LaplaceDiffusion1D(diffusivity(), equations)
+
+# Create DG solver with polynomial degree = 3 and (local) Lax-Friedrichs/Rusanov flux as surface flux
+solver = DGSEM(polydeg=3, surface_flux=flux_lax_friedrichs)
+
+coordinates_min = -1 # minimum coordinate
+coordinates_max =  1 # maximum coordinate
+
+# Create a uniformly refined mesh with periodic boundaries
+Base_lvl = 5
+mesh = TreeMesh(coordinates_min, coordinates_max,
+                initial_refinement_level=Base_lvl,
+                n_cells_max=30_000, # set maximum capacity of tree data structure
+                periodicity=true)
+
+function x_trans_periodic(x, domain_length = SVector(2*pi), center = SVector(0.0))
+  x_normalized = x .- center
+  x_shifted = x_normalized .% domain_length
+  x_offset = ((x_shifted .< -0.5*domain_length) - (x_shifted .> 0.5*domain_length)) .* domain_length
+  return center + x_shifted + x_offset
+end
+
+# Define initial condition
+function initial_condition_diffusive_convergence_test(x, t, equation::LinearScalarAdvectionEquation1D)
+  # Store translated coordinate for easy use of exact solution
+  x_trans = x_trans_periodic(x - equation.advection_velocity * t)
+  
+  nu = diffusivity()
+  c = 0.0
+  A = 1.0
+  L = 2
+  f = 1/L
+  omega = pi
+  scalar = c + A * sin(omega * sum(x_trans)) * exp(-nu * omega^2 * t)
+  return SVector(scalar)
+end
+initial_condition = initial_condition_diffusive_convergence_test
+
+@inline function reaction_term(u, x, t, equations)
+  return SVector(exp(-(u[1])^2))
+end
+  
+# define periodic boundary conditions everywhere
+boundary_conditions = boundary_condition_periodic
+boundary_conditions_parabolic = boundary_condition_periodic
+
+# A semidiscretization collects data structures and functions for the spatial discretization
+semi = SemidiscretizationHyperbolicParabolic(mesh, (equations, equations_parabolic), 
+  initial_condition_diffusive_convergence_test, solver;
+  boundary_conditions=(boundary_conditions, boundary_conditions_parabolic),
+  source_terms = reaction_term)
+
+###############################################################################
+# ODE solvers, callbacks etc.
+
+# Create ODE problem with time span from 0.0 to 1.0
+tspan = (0.0, 0.5)
+ode = semidiscretize(semi, tspan; split_form = false);
+
+# At the beginning of the main loop, the SummaryCallback prints a summary of the simulation setup
+# and resets the timers
+summary_callback = SummaryCallback()
+
+# The AnalysisCallback allows to analyse the solution in regular intervals and prints the results
+analysis_callback = AnalysisCallback(semi, interval=100)
+
+# The AliveCallback prints short status information in regular intervals
+alive_callback = AliveCallback(analysis_interval=100)
+
+amr_controller = ControllerThreeLevel(semi, 
+                                      IndicatorMax(semi, variable=first),
+                                      base_level=Base_lvl,
+                                      med_level=Base_lvl+1, med_threshold=0.1,
+                                      max_level=Base_lvl+3, max_threshold=0.6)
+
+amr_callback = AMRCallback(semi, amr_controller,
+                           interval=5,
+                           adapt_initial_condition=true)
+
+# Create a CallbackSet to collect all callbacks such that they can be passed to the ODE solver
+callbacks = CallbackSet(summary_callback, analysis_callback, alive_callback, amr_callback)
+#callbacks = CallbackSet(summary_callback, analysis_callback, alive_callback)
+
+###############################################################################
+# run the simulation
+#=
+CFL = 0.4
+dt = 0.0279620120039908231 * CFL
+
+b1   = 0.5
+bS   = 1.0 - b1
+cEnd = 0.5/bS
+ode_algorithm = PERK_Multi(4, 3, "/home/daniel/git/MA/EigenspectraGeneration/Spectra/1D_Adv_Diff_Reac/", 
+                           bS, cEnd, stage_callbacks = ())
+=#
+
+
+CFL = 0.5 * 0.99
+dt = 0.0279620120039908231 * CFL
+S = 16
+
+
+CFL = 0.99
+dt = 0.0279620120039908231 * CFL
+S = 32
+
+
+b1   = 0.5
+bS   = 1.0 - b1
+cEnd = 0.5/bS
+
+ode_algorithm = PERK(S, "/home/daniel/git/MA/EigenspectraGeneration/Spectra/1D_Adv_Diff_Reac/", bS, cEnd)
+
+sol = Trixi.solve(ode, ode_algorithm, dt = dt, save_everystep=false, callback=callbacks);
+
+#=
+# OrdinaryDiffEq's `solve` method evolves the solution in time and executes the passed callbacks
+time_int_tol = 1.0e-10
+time_abs_tol = 1.0e-10
+sol = solve(ode, KenCarp4(autodiff=false), abstol=time_abs_tol, reltol=time_int_tol,
+            save_everystep=false, callback=callbacks)
+=#
+
+# Print the timer summary
+summary_callback()
+plot(sol)
+pd = PlotData1D(sol)
+plot!(getmesh(pd))
