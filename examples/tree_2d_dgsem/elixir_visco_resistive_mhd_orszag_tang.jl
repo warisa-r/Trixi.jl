454d41a873a0197c80e6b6b8e4753cb058da190e
Daniel_Doehring <daniel.doehring@rwth-aachen.de>
2023-10-19 17:22:57 +0200

    Visco-resistive MHD

---
+++ examples/tree_2d_dgsem/elixir_visco_resistive_mhd_orszag_tang.jl
@@ -0,0 +1,125 @@
+
+using OrdinaryDiffEq, Plots
+using Trixi
+
+###############################################################################
+# semidiscretization of the compressible ideal GLM-MHD equations
+prandtl_number() = 1.0
+
+#mu() = 1e-2
+mu() = 1e-3
+#eta() = 1e-2
+eta() = 1e-3
+
+equations = IdealGlmMhdEquations2D(5/3)
+equations_parabolic = ViscoResistiveMhd2D(equations, mu = mu(),
+                                          Prandtl = prandtl_number(),
+                                          eta = eta(),
+                                          gradient_variables = GradientVariablesPrimitive())
+
+"""
+    initial_condition_orszag_tang(x, t, equations::IdealGlmMhdEquations2D)
+
+The classical Orszag-Tang vortex test case. Here, the setup is taken from
+- https://onlinelibrary.wiley.com/doi/pdf/10.1002/fld.4681
+"""
+function initial_condition_orszag_tang(x, t, equations::IdealGlmMhdEquations2D)
+  # setup taken from Derigs et al. DMV article (2018)
+  # domain must be [0, 1] x [0, 1], γ = 5/3
+  rho = 1.0
+  v1 = -2 * sqrt(pi) * sin(x[2])
+  v2 =  2 * sqrt(pi) * sin(x[1])
+  v3 = 0.0
+  p = 15/4 + 0.25 * cos(4*x[1]) + 0.8 * cos(2*x[1])*cos(x[2]) - cos(x[1])*cos(x[2]) + 0.25 * cos(2*x[2])
+  B1 = -sin(x[2])
+  B2 =  sin(2.0*x[1])
+  B3 = 0.0
+  psi = 0.0
+  return prim2cons(SVector(rho, v1, v2, v3, p, B1, B2, B3, psi), equations)
+end
+initial_condition = initial_condition_orszag_tang
+
+surface_flux = (flux_lax_friedrichs, flux_nonconservative_powell)
+volume_flux  = (flux_central, flux_nonconservative_powell)
+basis = LobattoLegendreBasis(3)
+
+indicator_sc = IndicatorHennemannGassner(equations, basis,
+                                         alpha_max=0.5,
+                                         alpha_min=0.001,
+                                         alpha_smooth=true,
+                                         variable=density_pressure)
+volume_integral = VolumeIntegralShockCapturingHG(indicator_sc;
+                                                 volume_flux_dg=volume_flux,
+                                                 volume_flux_fv=surface_flux)
+
+solver = DGSEM(basis, surface_flux, volume_integral)
+
+coordinates_min = (0.0, 0.0)
+coordinates_max = (2*pi, 2*pi)
+mesh = TreeMesh(coordinates_min, coordinates_max,
+                initial_refinement_level=6,
+                n_cells_max=100000)
+
+
+semi = SemidiscretizationHyperbolicParabolic(mesh, (equations, equations_parabolic), initial_condition, solver)
+
+
+###############################################################################
+# ODE solvers, callbacks etc.
+
+tspan = (0.0, 0.5)
+ode = semidiscretize(semi, tspan)
+
+summary_callback = SummaryCallback()
+
+analysis_interval = 100
+analysis_callback = AnalysisCallback(semi, interval=analysis_interval)
+
+alive_callback = AliveCallback(analysis_interval=analysis_interval)
+
+amr_indicator = IndicatorHennemannGassner(semi,
+                                          alpha_max=0.5,
+                                          alpha_min=0.001,
+                                          alpha_smooth=false,
+                                          variable=density_pressure)
+amr_controller = ControllerThreeLevel(semi, amr_indicator,
+                                      base_level=3,
+                                      med_level =6, med_threshold=0.005,
+                                      max_level =9, max_threshold=0.1)
+amr_callback = AMRCallback(semi, amr_controller,
+                           interval=10,
+                           adapt_initial_condition=true,
+                           adapt_initial_condition_only_refine=true)
+
+cfl = 0.8
+stepsize_callback = StepsizeCallback(cfl=cfl)
+
+glm_speed_callback = GlmSpeedCallback(glm_scale=0.5, cfl=cfl)
+
+callbacks = CallbackSet(summary_callback,
+                        analysis_callback,
+                        alive_callback,
+                        amr_callback,
+                        stepsize_callback,
+                        glm_speed_callback)
+
+###############################################################################
+# run the simulation
+
+#=
+time_int_tol = 1e-12
+sol = solve(ode, RDPK3SpFSAL49(), dt = 1e-5,
+            save_everystep = false, callback = callbacks)
+=#
+
+sol = solve(ode, CarpenterKennedy2N54(williamson_condition=false),
+            dt=1.0, # solve needs some value here but it will be overwritten by the stepsize_callback
+            save_everystep=false, callback=callbacks);
+           
+summary_callback() # print the timer summary
+plot(sol)
+
+pd = PlotData2D(sol)
+plot(pd["rho"])
+plot(pd["p"])
+plot!(getmesh(pd))
--- src/equations/equations_parabolic.jl
+++ src/equations/equations_parabolic.jl
@@ -15,3 +15,9 @@ include("compressible_navier_stokes.jl")
 include("compressible_navier_stokes_1d.jl")
 include("compressible_navier_stokes_2d.jl")
 include("compressible_navier_stokes_3d.jl")
+
+# Resistive and visuous MHD
+abstract type AbstractViscoResistiveMhd{NDIMS, NVARS} <:
+              AbstractEquationsParabolic{NDIMS, NVARS} end
+include("visco_resistive_mhd_2d.jl")
+include("visco_resistive_mhd_3d.jl")
