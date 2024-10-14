e08f3e9d7c91730c1800c1f18e417ee4ecd58d26
Warisa <compund555@gmail.com>
2024-10-14 19:58:46 +0200

    change cfl number and now result is fine yayy

--- examples/tree_1d_dgsem/elixir_advection_embedded_perk3.jl
+++ examples/tree_1d_dgsem/elixir_advection_embedded_perk3.jl
@@ -43,7 +43,7 @@ analysis_interval = 100
 analysis_callback = AnalysisCallback(semi, interval = analysis_interval)
 
 # The StepsizeCallback handles the re-calculation of the maximum Δt after each time step
-stepsize_callback = StepsizeCallback(cfl = 2.5)
+stepsize_callback = StepsizeCallback(cfl = 1.0)
 
 alive_callback = AliveCallback(alive_interval = analysis_interval)
 
